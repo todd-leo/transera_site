8b72587959d6d3a2b8c14abacca641a52eef2961
todd.leo <todd.f.leo@gmail.com>
2025-10-01 20:42:04 +0800

    添加用户的管理页面

--- merchants/wagtail_hooks.py
+++ merchants/wagtail_hooks.py
@@ -4,11 +4,16 @@ from wagtail import hooks
 from wagtail.admin.menu import Menu, AdminOnlyMenuItem, SubmenuMenuItem
 
 from merchants.admin.merchants import MerchantViewSet
+from users.admin import UserViewSet
 
 
 @hooks.register("register_admin_viewset")
 def register_merchant_viewset():
     return MerchantViewSet()
+
+@hooks.register("register_admin_viewset")
+def register_user_viewset():
+    return UserViewSet()
 
 # 顶层分组：必须返回 SubmenuMenuItem（不要返回 Menu 本体）
 @hooks.register("register_admin_menu_item")
@@ -28,7 +33,17 @@ def register_merchant_manage_item():
         label="商户管理",
         url=index_url,
         icon_name="user",
-        order=120,  # 排序权重，数字越小越靠前
+        order=120  # 排序权重，数字越小越靠前
+    )
+
+@hooks.register("register_merchant_menu_item")
+def register_user_manage_item():
+    index_url = reverse(UserViewSet().get_url_name("index"))
+    return AdminOnlyMenuItem(
+        label="用户管理",
+        url=index_url,
+        icon_name="user",
+        order=110
     )
 
 @hooks.register("insert_global_admin_css")
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+from wagtail.admin.viewsets.model import ModelViewSet
 
-# Register your models here.
+from users.models import User as AppUser
+
+
+class UserViewSet(ModelViewSet):
+    model = AppUser
+    icon = "user"
+    menu_label = "用户管理"
+    name = "users"           # 用于 URL 前缀/命名空间
+    add_to_admin_menu = False  # 由分组来统一加到菜单
+    list_display = [
+        "id", "register_account", "register_email", "distributor_id",
+        "is_active", "last_login", "register_time",
+    ]
+    list_filter = ["is_active", "distributor_id"]
+    search_fields = ["register_email", "register_account", "wechat_name", "wechat_id"]
+    ordering = ["-register_time"]
+    exclude_form_fields = []
